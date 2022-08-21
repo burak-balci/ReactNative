fd9b593e53ba9e10ba6a10f638392142c98624c3
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-21 11:11:07 +0300

    musicApp is done

--- musicApp/src/App.js
+++ musicApp/src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, FlatList, StyleSheet} from 'react-native';
 import music_data from './music-data.json';
 import SongCard from './components/SongCard';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
+  const [list, setList] = useState(music_data);
   const renderSong = ({item}) => <SongCard song={item} />;
 
   const renderSeperator = () => <View style={styles.seperator}></View>;
 
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searchedText) > -1;
+    });
+
+    setList(filteredList);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.container}>
-        <FlatList
-          keyExtractor={item => item.id}
-          data={music_data}
-          renderItem={renderSong}
-          ItemSeparatorComponent={renderSeperator}
-        />
-      </View>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={item => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={renderSeperator}
+      />
     </View>
   );
 };
