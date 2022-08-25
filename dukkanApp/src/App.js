a8580aaff98e0fb5233ec23608ba82b56eb114ab
Burak <96511762+burak-balci@users.noreply.github.com>
2022-08-25 10:03:55 +0300

    app is done

--- dukkanApp/src/App.js
+++ dukkanApp/src/App.js
@@ -4,14 +4,16 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Products from './pages/Products';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Loading from './components/Loading';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const userSession = useSelector(s => s.user);
   const isAuthLoading = useSelector(s => s.isAuthLoading);
+  const dispatch = useDispatch();
 
   return (
     <NavigationContainer>
@@ -34,6 +36,14 @@ const App = () => {
               title: 'Dükkan',
               headerStyle: {backgroundColor: '#64b5f6'},
               headerTitleStyle: {color: 'white'},
+              headerRight: () => (
+                <Icon
+                  name="logout"
+                  size={30}
+                  color="white"
+                  onPress={() => dispatch({type: 'REMOVE_USER'})}
+                />
+              ),
             }}
             name="ProductsPage"
             component={Products}
--- dukkanApp/src/pages/Products/Products.js
+++ dukkanApp/src/pages/Products/Products.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {View, FlatList, Button} from 'react-native';
+import {View, FlatList} from 'react-native';
 import ProductCard from '../../components/ProductCard';
 import useFetch from '../../hooks/useFetch';
 import Loading from '../../components/Loading';
 import Error from '../../components/Error';
-import {useDispatch} from 'react-redux';
 
 const Products = ({navigation}) => {
-  const dispatch = useDispatch;
   const {loading, data, error} = useFetch(process.env.API_URL);
 
   const handleProductSelect = id => {
@@ -28,10 +26,6 @@ const Products = ({navigation}) => {
 
   return (
     <View>
-      <Button
-        title="Log out"
-        onPress={() => dispatch({type: 'SET_USER', payload: {user: null}})}
-      />
       <FlatList data={data} renderItem={renderProduct} />
     </View>
   );
